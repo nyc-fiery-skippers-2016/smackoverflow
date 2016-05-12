3b8b41d0389208d231beb16f1f7042af0c98d6bf
Esther Leytush <eleytush@gmail.com>
2016-05-12 16:55:06 -0400

    comments and answers now show up bc of new methods in model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -16,4 +16,14 @@ class Comment < ActiveRecord::Base
     return body if body.split.length <= 10
     body.split[0..10].join(" ") + "..."
   end
+
+  def question
+    # find the question page on whcih this comment lives
+    if commentable_type == "Question"
+      Question.find_by(id: commentable_id)
+    else
+      answer = Answer.find_by(id: commentable_id)
+      Question.find_by(id: answer.id)
+    end
+  end
 end
