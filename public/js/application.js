c9b7f1e1cbd30fd06a7a751d32c5fc3cdb4f74c0
Lisa Buch <email": null>
2016-05-12 13:12:26 -0400

    fixing partials, completed on-page ajaxifyied editing

--- public/js/application.js
+++ public/js/application.js
@@ -1,21 +1,21 @@
-$(function(){
+$(document).ready(function(){
   $("#edit").on('click', function(e){
     e.preventDefault();
+
     $("#editform").show();
   });
 
   $("#editform").on('submit', function(e){
     e.preventDefault();
-    question_id = $(this).parent().parent().attr('id');
+    question_id = $(this).parent().attr('id');
     var data = $(e.target).serialize();
     var url = e.target.action;
     var type = e.target.method;
     $.ajax({
-      data = data,
-      url = url,
-      type = type
+      data: data,
+      url: url,
+      type: type
     }).done(function(response){
-      console.log(response);
       $("#" + question_id).html(response);
     });
   });
